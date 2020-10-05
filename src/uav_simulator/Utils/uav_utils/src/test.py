b4a71d6ffd893db9c211b39b041b71dd7f00765d
Kadhir Umasankar <kadhirus99@utexas.edu>
2020-10-04 22:53:13 -0500

    Added a message to be published but it's not publishing TODO: Figure out why message isn't being published

--- src/uav_simulator/Utils/uav_utils/src/test.py
+++ src/uav_simulator/Utils/uav_utils/src/test.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python2
 
 import rospy
-from nav_msgs.msg import Path
 from geometry_msgs.msg import PoseStamped
 
 
@@ -10,29 +9,24 @@ class WaypointFeed(object):
     def __init__(self):
         print('[INFO] Initializing object...')
         # COMBAK: temporarily setting queue_size to 1
-        self.waypoint_pub = rospy.Publisher('/waypoint_generator/waypoints', Path, queue_size=10)
-        waypoint_msg = Path()
-        waypoint_msg.header.seq = 0
+        self.waypoint_pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=1)
+        waypoint_msg = PoseStamped()
+        waypoint_msg.header.seq = 1
         waypoint_msg.header.stamp = rospy.Time.now()
         waypoint_msg.header.frame_id = "world"
-        pose = PoseStamped()
-        pose.header.seq = 0
-        pose.header.stamp = rospy.Time.now()
-        pose.header.frame_id = "world"
-        pose.pose.position.x = 10.1873664856
-        pose.pose.position.y = -13.2499380112
-        pose.pose.position.z = 0
-        pose.pose.orientation.w = 1.0
-        waypoint_msg.poses.append(pose)
+        waypoint_msg.pose.position.x = 10.1873664856
+        waypoint_msg.pose.position.y = -13.2499380112
+        waypoint_msg.pose.position.z = 0
+        waypoint_msg.pose.orientation.w = 1.0
         print(waypoint_msg)
         self.waypoint_pub.publish(waypoint_msg)
-        print('[INFO] Published')
+        print('[INFO] Message published')
 
 
 def main():
     print('[INFO] Running file')
     rospy.init_node('waypoint_feed_node', anonymous=True)
-    camera_feed_object = WaypointFeed()
+    waypoint_feed_object = WaypointFeed()
     try:
         rospy.spin()
     except KeyboardInterrupt:
